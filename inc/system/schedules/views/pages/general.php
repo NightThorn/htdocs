c8099aa6b58593aebc5acbf14bc730a7bbaeb347
greasygamerdotcom <greasygamer@greasygamer.com>
2022-06-14 14:50:59 -0500

    box shadow

--- inc/system/schedules/views/pages/general.php
+++ inc/system/schedules/views/pages/general.php
@@ -1,3 +1,5 @@
-<div class="row lg">
-	<div class="calendar" id="schedule-calendar" data-result="html" data-content="column-three"></div>
-</div>
+	<div class="row lg">
+
+			<div class="calendar" id="schedule-calendar" data-result="html" data-content="column-three"></div>
+		</div>
+	
